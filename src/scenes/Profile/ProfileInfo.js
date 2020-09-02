18cce78e7b3275492fe4025407cb5783c0bce275
Inna <inna@Innas-Mac-mini.local>
2020-09-02 13:44:04 +0300

    [board#9] auto setting chat interval

--- src/scenes/Profile/ProfileInfo.js
+++ src/scenes/Profile/ProfileInfo.js
@@ -70,13 +70,13 @@ class ProfileInfoScene extends Component {
     Animated.timing(
       this.spinValue,
       {  toValue: 1, duration: 4000 }
-    ).start()
+    ).start();
   }
 
   componentWillUnmount(){
     const { updateUser, getUser } = this.props;
     const { avatar, user, language, font_size, speed } = this.state;
-    AsyncStorage.setItem('speed', speed)
+    AsyncStorage.setItem('speed', speed);
     const dbUser = getUser();
     if(dbUser.sync_id !== user.sync_id) return;
     const { input } = this.state;
@@ -210,7 +210,9 @@ class ProfileInfoScene extends Component {
               </Option>
 
               <Option title={strings.chatIntervalLabel} style={{marginTop:5}}>
-                <TextInput
+                <Picker
+                  items={constant.chatInterval}
+                  onValueChange={(speed) => this.setState({speed})}
                   value={speed}
                   style={{ iconContainer:{ top: 5 }, inputIOS:{ paddingVertical:10, color:colors.white }, inputAndroid:{ color:colors.white} }}
                   Icon={() => (<Icon style={{fontFamily:'meinobjekt', fontSize:24, color:colors.white}}>c</Icon>)}
