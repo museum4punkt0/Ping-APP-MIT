5723cedfc4120ddb335327f74b4e60bf07c48e92
Denys Bondar <denys.bodnar@osl.com>
2020-11-05 16:26:49 +0200

    mo-103: Added platform check, udate images path only for android

--- src/components/Chat/ChatDialog.js
+++ src/components/Chat/ChatDialog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, Platform } from 'react-native';
 import PropTypes from 'prop-types';
 import Text from '../Text'
 import styles, {colors} from '../../config/styles';  
@@ -33,7 +33,7 @@ ImageMessageContent.propTypes = ({
         {isIncoming && <View style={{flex: 1, minWidth: 40}} />}
         <View style={{minWidth: 40}}>
           {((!isIncoming && message.responses && message.responses.length > 0)||(last && !isIncoming)) && 
-          <Image style={{borderRadius:16, width:32, height:32, backgroundColor:colors.dark}} source={{uri: getImage(avatar) + '?' + new Date()}} />}
+          <Image style={{borderRadius:16, width:32, height:32, backgroundColor:colors.dark}} source={{uri: getImage(avatar) + (Platform.OS === 'ios' ? '' : '?' + new Date())}} />}
         </View>
         <View style={[styles.chat.messageTextContainer, { backgroundColor: color }]}>
           {renderMessageContent()}
